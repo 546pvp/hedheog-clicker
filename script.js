02e322d96b71912acf5860f9047abd6bcc5304f6
546pvp <79662987+546pvp@users.noreply.github.com>
2021-11-05 16:45:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var clicker = getElementById("clicker")
 var i = 0
 var message = `You tapped ${i} times!`
-while ( i < 100 ) {
+clicker.oncclick = function(e) {
+  while ( i < 100 ) {
   message = `You tapped ${i} times!
   alert(message)
   i++
+ }
 }
